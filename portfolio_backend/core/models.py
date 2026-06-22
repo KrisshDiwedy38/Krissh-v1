60dfe8629be0611352bbef199472ebc5619ed641
Krissh <krisshdiwedy38@gmail.com>
2026-06-23 04:38:35 +0530

    feat(backend/core): add AboutMe and Skill models to database schema

--- portfolio_backend/core/models.py
+++ portfolio_backend/core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from core.validators import validate_image
 
-# Create your models here.
+class AboutMe(models.Model):
+    bio = models.TextField()
+    profile_image = models.ImageField(upload_to='about/', validators=[validate_image], null=True, blank=True)
+    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
+    title = models.CharField(max_length=100, default='The Architect')
+    core_skill = models.CharField(max_length=100, default='Celestial Neobrutalism')
+    weapon = models.CharField(max_length=100, default='Modern Frontend & Shaders')
+    base = models.CharField(max_length=100, default='Digital Nebula 0x4')
+
+    class Meta:
+        verbose_name_plural = "About Me"
+
+    def __str__(self):
+        return "About Me Configuration"
+
+    def save(self, *args, **kwargs):
+        # Enforce singleton
+        if AboutMe.objects.exists() and not self.pk:
+            self.pk = AboutMe.objects.first().pk
+        super(AboutMe, self).save(*args, **kwargs)
+
+class SkillCategory(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100, blank=True)
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['order', 'id']
+        verbose_name_plural = "Skill Categories"
+
+    def __str__(self):
+        return self.title
+
+class Skill(models.Model):
+    category = models.ForeignKey(SkillCategory, related_name='skills', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['order', 'id']
+
+    def __str__(self):
+        return self.name
